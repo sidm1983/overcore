0e6ca4221e2cb3385ba8af356b39ac1ae623ed49
Sid Mansukhani <sid.mansukhani@isobar.com>
2019-04-04 01:53:28 +1100

    Added Initial set of files which include dotnet core project/solution files as well as one extension function and its associated unit test.

---
+++ corex.string/extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace corex.@string.extensions
+{
+    public static class StringExtensions
+    {
+        public static T To<T>(this string input)
+        {
+            return (T) Convert.ChangeType(input, typeof(T));
+        }
+    }
+}
