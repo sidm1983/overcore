0cdd0179ce0ba4b19c2b2544ebbb1477f61b5331
Sid Mansukhani <sid1983@gmail.com>
2019-04-06 01:42:02 +1100

    Added more tests for the 'StringExtensions.To<T>()' extension method including a couple of negative tests.

--- corex.string/extensions/StringExtensions.cs
+++ corex.string/extensions/StringExtensions.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-
+[assembly: CLSCompliant(true)]
 namespace corex.@string.extensions
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// This extension method converts a string to a Common Language Runtime (CLR) type that has an equivalent value.
+        /// </summary>
+        /// <param name="input">The string to convert</param>
+        /// <typeparam name="T">The CLR type into which the input string will be converted</typeparam>
+        /// <returns>An instance of the CLR type that has an equivalent value to the input string</returns>
         public static T To<T>(this string input)
         {
             return (T) Convert.ChangeType(input, typeof(T));
