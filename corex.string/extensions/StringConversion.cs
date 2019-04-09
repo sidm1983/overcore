f6691b422aec55d232d3d034f1b86331c9d4bb19
Sid Mansukhani <sid1983@gmail.com>
2019-04-10 00:39:07 +1000

    Add an overload for the To<T>() method that accepts a default value which is returned upon conversion failure.

--- corex.string/extensions/StringConversion.cs
+++ corex.string/extensions/StringConversion.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 [assembly: CLSCompliant(true)]
 namespace corex.@string.extensions
 {
+    /// <summary>
+    /// A static class containing extension methods that convert a string into other types.
+    /// </summary>
     public static class StringConversion
     {
         /// <summary>
@@ -11,9 +14,30 @@ namespace corex.@string.extensions
         /// <param name="input">The string to convert</param>
         /// <typeparam name="T">The CLR type into which the input string will be converted</typeparam>
         /// <returns>An instance of the CLR type that has an equivalent value to the input string</returns>
-        public static T To<T>(this string input)
+        public static T To<T>(this string input) => (T) Convert.ChangeType(input, typeof(T));
+
+        /// <summary>
+        /// This extension method converts a string to a Common Language Runtime (CLR) type that has an equivalent value.
+        /// Additionally, this method also accepts a default value which is returned if the string cannot be converted to the specified type.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="defaultValue"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T To<T>(this string input, T defaultValue)
         {
-            return (T) Convert.ChangeType(input, typeof(T));
+            try
+            {
+                var output = To<T>(input);
+
+                if (output != null)
+                {
+                    return output;
+                }
+            }
+            catch {} //Intentionally ignoring any exceptions as we just want it to return the default value that was passed in if an exception does occur.
+
+            return defaultValue;
         }
     }
 }
